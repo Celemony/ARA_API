d78668f6b972b9ee93c5278a6980e07545899d1d
sgretscher <41306803+sgretscher@users.noreply.github.com>
2023-10-27 10:34:07 +0200

    Increase required minimum CMake version to 3.19 (same as VST3)

--- ARA_API_Helpers.cmake
+++ ARA_API_Helpers.cmake
@@ -18,41 +18,6 @@
 
 include_guard()
 
-# ======================
-# private helper function needed to deal with various different CMake versions - do not call externally!
-
-function(ara_private_configure_ARA_API_interface_settings target visibility)
-    target_include_directories(${target}
-        ${visibility}
-            "${CMAKE_CURRENT_LIST_DIR}/.."
-    )
-
-    # \todo test this - do we always see the correct value for ARA_ENABLE_DEPRECATION_WARNINGS?
-    if(ARA_ENABLE_DEPRECATION_WARNINGS)
-        target_compile_definitions(${target}
-            ${visibility}
-                ARA_ENABLE_DEPRECATION_WARNINGS=1
-        )
-    endif()
-endfunction()
-
-
-# ======================
-# "link" given target against ARA_API
-# a function is provided to encapsulate workarounds for older CMake versions
-
-function(ara_link_ARA_API target visibility)
-
-    if(CMAKE_VERSION VERSION_GREATER_EQUAL 3.12)
-        target_link_libraries(${target} ${visibility} ARA_API)
-    else()
-        # older CMake can't automatically pull the public properties from object targets,
-        # therefore we need to set the relevant properties from ARA_API manually here.
-        ara_private_configure_ARA_API_interface_settings(${target} ${visibility})
-    endif()
-
-endfunction()
-
 
 # ======================
 # apply the recommended warning configuration for our targets
